ac733206d11e02eb16a187387f8afb6315257589
Siena Moon <soliloquy.moon@gmail.com>
2024-09-16 02:24:13 -0500

    Declare an ImageView variable

--- app/src/main/java/com/cs407/helloworld/MainActivity.kt
+++ app/src/main/java/com/cs407/helloworld/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    // Declare an ImageView variable
+    private lateinit var ingView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
